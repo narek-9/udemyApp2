7a4c18b5970966feb0d98bb3de6ceae5367e5e7a
Narek <narekazibekyan5@gmail.com>
2023-02-04 23:35:58 +0400

    Lesson 79

--- js/script.js
+++ js/script.js
@@ -146,12 +146,13 @@ window.addEventListener("DOMContentLoaded", () => {
    // Используем классы для карточек
 
    class MenuCard {
-      constructor(src, alt, title, descr, price, parentSelector) {
+      constructor(src, alt, title, descr, price, parentSelector, ...classes) {
          this.src = src;
          this.alt = alt;
          this.title = title;
          this.descr = descr;
          this.price = price;
+         this.classes = classes;
          this.parent = document.querySelector(parentSelector);
          this.transfer = 27;
          this.changeToUAH();
@@ -162,7 +163,15 @@ window.addEventListener("DOMContentLoaded", () => {
       }
       render() {
          const element = document.createElement("div");
-         element.innerHTML = `<div class="menu__item">
+         if (!this.classes.length) {
+            this.element = "menu__item";
+            element.classList.add(this.element);
+         } else {
+            this.classes.forEach((className) =>
+               element.classList.add(className)
+            );
+         }
+         element.innerHTML = `
             <img src=${this.src} alt=${this.alt} />
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">
@@ -174,8 +183,7 @@ window.addEventListener("DOMContentLoaded", () => {
                <div class="menu__item-total">
                   <span>${this.price}</span> грн/день
                </div>
-            </div>
-         </div>`;
+            </div>`;
          this.parent.append(element);
       }
    }
