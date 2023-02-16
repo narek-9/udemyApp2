da8d040b2ba3f5578b46c5b576041092517ceffb
Narek <narekazibekyan5@gmail.com>
2023-02-16 19:04:17 +0400

    Lesson 85

--- js/script.js
+++ js/script.js
@@ -95,8 +95,7 @@ window.addEventListener("DOMContentLoaded", () => {
    // Modal
 
    const modalTrigger = document.querySelectorAll("       [data-modal]"),
-      modal = document.querySelector(".modal"),
-      modalCloseBtn = document.querySelector("[data-close]");
+      modal = document.querySelector(".modal");
 
    function openModal() {
       modal.classList.add("show");
@@ -115,10 +114,8 @@ window.addEventListener("DOMContentLoaded", () => {
       document.body.style.overflow = "";
    }
 
-   modalCloseBtn.addEventListener("click", closeModal);
-
    modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
+      if (e.target === modal || e.target.getAttribute("data-close") == "") {
          closeModal();
       }
    });
@@ -129,7 +126,7 @@ window.addEventListener("DOMContentLoaded", () => {
       }
    });
 
-   const modalTimerId = setTimeout(openModal, 5000);
+   const modalTimerId = setTimeout(openModal, 50000);
 
    function showModalScroll() {
       if (
@@ -219,28 +216,31 @@ window.addEventListener("DOMContentLoaded", () => {
    const forms = document.querySelectorAll("form");
 
    const message = {
-      loading: "Загрузка",
+      loading: "img/form/spinner.svg",
       success: "Спасибо! Скоро мы с вами свяжемся",
       failure: "Что-то пошло не так․․․",
    };
 
    forms.forEach((item) => {
-      postdata(item);
+      postData(item);
    });
 
-   function postdata(form) {
+   function postData(form) {
       form.addEventListener("submit", (e) => {
          e.preventDefault();
 
-         const statusMessage = document.createElement("div");
-         statusMessage.classList.add("status");
-         statusMessage.textContent = message.loading;
-         form.append(statusMessage);
+         const statusMessage = document.createElement("img");
+         statusMessage.src = message.loading;
+         statusMessage.style.cssText = `
+            display: block;
+            margin: 0 auto;
+         `;
+         form.insertAdjacentElement("afterend", statusMessage);
 
          const request = new XMLHttpRequest();
          request.open("POST", "server.php");
 
-         request.setRequestHeader("Content-type", "application/json");
+         request.setRequestHeader("Content-type", "application/json; charset=utf-8");
          const formData = new FormData(form);
 
          const object = {};
@@ -249,21 +249,42 @@ window.addEventListener("DOMContentLoaded", () => {
          });
 
          const json = JSON.stringify(object);
-
+         
          request.send(json);
 
          request.addEventListener("load", () => {
             if (request.status === 200) {
                console.log(request.response);
-               statusMessage.textContent = message.success;
+               showThanksModal(message.success);
                form.reset();
-               setTimeout(() => {
-                  statusMessage.remove();
-               }, 2000);
+               statusMessage.remove();
             } else {
-               statusMessage.textContent = message.failure;
+               showThanksModal(message.failure);
             }
          });
       });
+   }
+
+   function showThanksModal(message) {
+      const prevModalDialog = document.querySelector(".modal__dialog");
+
+      prevModalDialog.classList.add("hide");
+      openModal();
+
+      const thanksModal = document.createElement("div");
+      thanksModal.classList.add("modal__dialog");
+      thanksModal.innerHTML = `
+         <div clas="modal__content">
+            <div class="modal__close" data-close>×</div>
+            <div class="modal__title">${message}</div>
+         </div>
+      `;
+      document.querySelector(".modal").append(thanksModal);
+      setTimeout(() => {
+         thanksModal.remove();
+         prevModalDialog.classList.add("show");
+         prevModalDialog.classList.remove("hide");
+         closeModal();
+      }, 4000);
    }
 });
