95f8ee6555a76a37943f271b1d6e4fd245b85afb
Narek <narekazibekyan5@gmail.com>
2023-02-17 14:26:40 +0400

    Lesson 87

--- js/script.js
+++ js/script.js
@@ -237,10 +237,6 @@ window.addEventListener("DOMContentLoaded", () => {
          `;
          form.insertAdjacentElement("afterend", statusMessage);
 
-         const request = new XMLHttpRequest();
-         request.open("POST", "server.php");
-
-         request.setRequestHeader("Content-type", "application/json; charset=utf-8");
          const formData = new FormData(form);
 
          const object = {};
@@ -248,20 +244,26 @@ window.addEventListener("DOMContentLoaded", () => {
             object[key] = value;
          });
 
-         const json = JSON.stringify(object);
-         
-         request.send(json);
-
-         request.addEventListener("load", () => {
-            if (request.status === 200) {
-               console.log(request.response);
+         fetch("server.php", {
+            method: "POST",
+            headers: {
+               "Content-type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify(object),
+         })
+            .then((data) => data.text())
+            .then((data) => {
+               console.log(data);
                showThanksModal(message.success);
                form.reset();
                statusMessage.remove();
-            } else {
+            })
+            .catch(() => {
                showThanksModal(message.failure);
-            }
-         });
+            })
+            .finally(() => {
+               form.reset();
+            });
       });
    }
 
